f73991a50d8ccec07b2b6a5496ac34d00f729af6
Jari Pennanen <ciantic@oksidi.com>
2025-05-01 23:31:24 +0300

    renaming, pack unpack makes more sense

--- bitgen/src/implementer.rs
+++ bitgen/src/implementer.rs
@@ -18,6 +18,7 @@ use crate::destruct::destruct;
 use crate::destruct::Destructurer;
 use crate::destruct::DestructurerCbArg;
 
+/// Packs unit struct id bytes
 fn build_unitstruct_packer(attrs: &Vec<Attribute>, name: &Type) -> TokenStream {
     let mut ret = quote! {};
     if let Some(idbytes) = find_attr_by_name(&attrs, "id") {
@@ -54,7 +55,6 @@ fn build_object_unpacker(attrs: &Vec<Attribute>, object_name: &Ident) -> TokenSt
 fn build_object_packer(attrs: &Vec<Attribute>) -> TokenStream {
     let mut ret = quote! {};
     
-
     if let Some(expr) = find_attr_by_name(&attrs, "prepend_length") {
         ret.extend(quote! {
             bytes
@@ -160,7 +160,7 @@ fn get_id_bytes(variant: &syn::Variant) -> IdBytes {
     panic!("No id found for variant {:?}", variant.to_token_stream());
 }
 
-fn construct_callback(arg: &ConstructorCbArg) -> TokenStream {
+fn unpacking_callback(arg: &ConstructorCbArg) -> TokenStream {
     let field = &arg.field;
     let type_name = &arg.type_name;
 
@@ -168,22 +168,17 @@ fn construct_callback(arg: &ConstructorCbArg) -> TokenStream {
         FieldDef::Named { attrs, .. } => build_field_unpacker(&attrs, None),
         FieldDef::Unnamed { attrs, .. } => build_field_unpacker(&attrs, None),
         FieldDef::UnitStruct { .. } => quote! { #type_name },
-        FieldDef::UnitEnum {
-            variant_name,
-            ..
-        } => quote! { #type_name::#variant_name },
+        FieldDef::UnitEnum { variant_name, .. } => quote! { #type_name::#variant_name },
     }
 }
 
-fn destruct_callback(args: &DestructurerCbArg) -> TokenStream {
+fn packing_callback(args: &DestructurerCbArg) -> TokenStream {
     // let var_name = &args.var_name;
     let type_name = &args.type_name;
 
     match &args.field {
         FieldDef::Named { attrs, name, .. } => build_field_packer(attrs, name),
-        FieldDef::Unnamed {
-            attrs, var_match, ..
-        } => build_field_packer(attrs, var_match),
+        FieldDef::Unnamed { attrs, var_match, .. } => build_field_packer(attrs, var_match),
         FieldDef::UnitStruct { attrs } => build_unitstruct_packer(attrs, type_name),
         FieldDef::UnitEnum {..} => quote! {},
     }
@@ -193,40 +188,53 @@ pub fn implementer(items: &Vec<Item>) -> Vec<proc_macro2::TokenStream> {
     let impls = items
         .iter()
         .filter_map(|item| {
-            let (name, destructed, constructed, prepend_dest, prepend_const) = match item {
+            match item {
                 Item::Struct(istruct) => {
                     let genitem = GenItem::Struct(istruct.clone());
                     let struct_name = &istruct.ident;
-                    let prepend_dest = build_object_packer(&istruct.attrs);
-                    let prepend_const = build_object_unpacker(&istruct.attrs, struct_name);
-                    let destructed = destruct(&Destructurer {
+                    let object_packer = build_object_packer(&istruct.attrs);
+                    let object_unpacker = build_object_unpacker(&istruct.attrs, struct_name);
+                    let pack_fields = destruct(&Destructurer {
                         item: genitem.clone(),
                         wrapper: |fields| quote! {
                             #(#fields)*
                         },
-                        destructrurer: destruct_callback,
+                        destructrurer: packing_callback,
                     });
-                    let constructed = construct(&Constructor {
+                    let unpack_to_value = construct(&Constructor {
                         item: genitem.clone(),
-                        constructer: construct_callback,
+                        constructer: unpacking_callback,
                     });
-                    (struct_name, destructed, quote! {
-                        Ok(#constructed)
-                    }, prepend_dest, prepend_const)
+
+                    Some(quote! {
+                        impl FromToPacket for #struct_name {
+                            fn from_packet(bytes: &mut Packet) -> Result<Self, PacketError> {
+                                #object_unpacker
+                                Ok(#unpack_to_value)
+                            }
+                            fn to_packet(&self, bytes: &mut Packet) -> Result<(), PacketError> {
+                                #object_packer
+                                match self {
+                                    #pack_fields
+                                };
+                                Ok(())
+                            }
+                        }
+                    })
                 }
                 Item::Enum(ienum) => {
                     let enum_name = &ienum.ident;
-                    let prepend_dest = build_object_packer(&ienum.attrs);
-                    let prepend_const = build_object_unpacker(&ienum.attrs, enum_name);
-                    let mut destructed = Vec::new();
-                    let mut constructed = Vec::new();
+                    let object_packer = build_object_packer(&ienum.attrs);
+                    let object_unpacker = build_object_unpacker(&ienum.attrs, enum_name);
+                    let mut packers = Vec::new();
+                    let mut unpackers = Vec::new();
                     let last_variant = ienum.variants.last().unwrap();
                     let last_variant_id = get_id_bytes(&last_variant);
                     for variant in &ienum.variants {
                         
                         // Match variant id bytes (&[0x01, 0x02] or _ which is the passthrough)
                         let id_bytes = get_id_bytes(variant);
-                        let destruct_id_bytes = match &id_bytes {
+                        let pack_id_bytes = match &id_bytes {
                             IdBytes::Bytes(id_bytes) => Some(quote! {
                                 bytes.pack_bytes(#id_bytes)?;
                             }),
@@ -236,29 +244,27 @@ pub fn implementer(items: &Vec<Item>) -> Vec<proc_macro2::TokenStream> {
                         };
 
                         let genitem = GenItem::Enum(ienum.clone(), variant.clone());
-                        let constr = construct(&Constructor {
+                        let unpack = construct(&Constructor {
                             item: genitem.clone(),
-                            constructer: construct_callback,
+                            constructer: unpacking_callback,
                         });
-                        let destr = destruct(&Destructurer {
+                        let pack = destruct(&Destructurer {
                             item: genitem.clone(),
                             wrapper: |fields| quote! {
-                                #destruct_id_bytes
+                                #pack_id_bytes
                                 #(#fields)*
                             },
-                            destructrurer: destruct_callback,
+                            destructrurer: packing_callback,
                         });
-                        destructed.push(quote! {
-                            #destr
-                        });
-                        constructed.push(match id_bytes {
-                            IdBytes::Bytes(token_stream) =>    quote! {
+                        packers.push(pack);
+                        unpackers.push(match id_bytes {
+                            IdBytes::Bytes(token_stream) => quote! {
                                 if bytes.next_if_eq(#token_stream) {
-                                    return Ok(#constr);
+                                    return Ok(#unpack);
                                 }
                             },
                             IdBytes::Passthrough => quote! {
-                                Ok(#constr)
+                                Ok(#unpack)
                             },
                         });
                     }
@@ -267,42 +273,30 @@ pub fn implementer(items: &Vec<Item>) -> Vec<proc_macro2::TokenStream> {
                         IdBytes::Bytes(_) => quote! {
                             Err(PacketError::Unspecified(format!("No matching variant found for {}", stringify!(#enum_name))))
                         },
-                        IdBytes::Passthrough => quote! {
-                            
-                        },
+                        IdBytes::Passthrough => quote! {},
                     };
 
-                    (
-                        enum_name,
-                        quote! {
-                            #(#destructed)*
-                        },
-                        quote! {
-                            #(#constructed);*
-                            #err_or_nothing
-                        },
-                        prepend_dest,
-                        prepend_const
-                    )
+
+                    Some(quote! {
+                        impl FromToPacket for #enum_name {
+                            fn from_packet(bytes: &mut Packet) -> Result<Self, PacketError> {
+                                #object_unpacker
+                                #(#unpackers);*
+                                #err_or_nothing
+                            }
+                            fn to_packet(&self, bytes: &mut Packet) -> Result<(), PacketError> {
+                                #object_packer
+                                match self {
+                                    #(#packers)*
+                                };
+                                Ok(())
+                            }
+                        }
+                    })
                 }
                 _ => return None,
-            };
+            }
 
-            Some(quote! {
-                impl FromToPacket for #name {
-                    fn from_packet(bytes: &mut Packet) -> Result<Self, PacketError> {
-                        #prepend_const
-                        #constructed
-                    }
-                    fn to_packet(&self, bytes: &mut Packet) -> Result<(), PacketError> {
-                        #prepend_dest
-                        match self {
-                            #destructed
-                        };
-                        Ok(())
-                    }
-                }
-            })
         })
         .collect();
 
